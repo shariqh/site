8a519ca4d11e790dbdf1efa05a081f75570a17b8
Shariq Hirani <shariqhirani2010@gmail.com>
2021-07-04 17:37:25 -0500

    completed initial setup, added tailwind dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    purge: [
+        // Use *.tsx if using TypeScript
+        './pages/**/*.js',
+        './components/**/*.js'
+    ],
+    theme: {},
+    variants: {},
+    plugins: [],
+}
