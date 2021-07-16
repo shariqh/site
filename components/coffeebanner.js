b1ae460d43dc2730896a5a144c6b4557b563e250
Shariq Hirani <9244782+shariqh@users.noreply.github.com>
2021-07-16 16:29:20 -0500

    added icons

--- components/coffeebanner.js
+++ components/coffeebanner.js
@@ -36,8 +36,7 @@ const Coffeebanner = ({index, coffee}) => {
 
     return (
         <div className={`sm:p-12 ${isFeatured ? "md:py-48" : "md:py-12"} bg-opacity-30 bg-${coffee.bg_color}`}>
-            <div
-                className={`p-4 mx-auto flex flex-col ${isOdd ? "md:flex-row-reverse" : "md:flex-row"} justify-between space-x-4 max-w-screen-lg`}>
+            <div className={`p-4 mx-auto flex flex-col ${isOdd ? "md:flex-row-reverse" : "md:flex-row"} justify-between space-x-4 max-w-screen-lg`}>
                 <div className="flex items-center p-4">
                     <div>
                         {isFeatured && (
@@ -77,6 +76,36 @@ const Coffeebanner = ({index, coffee}) => {
                                 <text>dark</text>
                             </div>
                         </div>
+                        <div className="space-y-1 pt-4 flex flex-row justify-between text-lg">
+                            {/*<div>*/}
+                            {/*    <p className="flex flex-row">*/}
+                            {/*        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"*/}
+                            {/*             viewBox="0 0 24 24" stroke="currentColor">*/}
+                            {/*            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}*/}
+                            {/*                  d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>*/}
+                            {/*        </svg>*/}
+                            {/*        &nbsp;{price}*/}
+                            {/*    </p>*/}
+                            {/*</div>*/}
+                            <a href={coffee.purchase_link}>
+                                <p className="flex flex-row">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
+                                         viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"/>
+                                    </svg>
+                                    &nbsp;Buy Now
+                                </p>
+                            </a>
+                            <a href={coffee.product_link}>
+                                <p className="flex flex-row">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+                                    &nbsp;Product Page
+                                </p>
+                            </a>
+                        </div>
                     </div>
                 </div>
                 <div>
@@ -91,38 +120,6 @@ const Coffeebanner = ({index, coffee}) => {
                                 layout="fill"
                                 sizes="100%"/>
                         )}
-                    </div>
-                    <div className="pt-12 space-y-1 flex flex-col float-right text-right">
-                        <div>
-                            <p className="flex flex-row">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
-                                     viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                                          d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
-                                </svg>
-                                &nbsp;{price}
-                            </p>
-                        </div>
-                        <a href={coffee.purchase_link}>
-                            <p className="flex flex-row">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
-                                     viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                          d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"/>
-                                </svg>
-                                &nbsp;Buy Now
-                            </p>
-                        </a>
-                        <a href={coffee.product_link}>
-                            <p className="flex flex-row">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
-                                     viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                                          d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
-                                </svg>
-                                &nbsp;Product Page
-                            </p>
-                        </a>
                     </div>
                 </div>
             </div>
